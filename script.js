cd690bce2a5898229663753ad8c01d307419040c
Russ Dudek <rdudek@Duder1.local>
2024-06-08 00:29:15 -0400

    Offset

--- script.js
+++ script.js
@@ -158,7 +158,7 @@ function setupToggleButtons() {
 
 function setupSmoothScroll() {
     console.log("Setting up smooth scroll...");
-    const headerOffset = document.querySelector('header').offsetHeight + 60; // Add extra offset
+    const headerOffset = document.querySelector('header').offsetHeight + 100; // Add extra offset
 ; // Get the header height
 
     document.querySelectorAll('.scroll-to').forEach(anchor => {
